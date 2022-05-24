2a0ffdc071bde4a8d38f14e9e3fb34052c3e171d
Claude-Zq <1574089391@qq.com>
2022-05-24 10:40:03 +0800

    定义了跑步记录封装成一个结构体

---
+++ record.h
@@ -0,0 +1,17 @@
+//
+// Created by 周青 on 2022/5/24.
+//
+
+#pragma once
+#include "date.h"
+
+typedef enum cate{
+    EasyRunning = 1,LongSlowDistance,MarathonPaceRunning,ThresholdRunning,IntervalTraining
+}CATE;
+
+typedef struct record{
+    DATE date; //年月日
+    float duration;//单位(h)
+    float distance;//单位(km)
+    CATE category;//类别
+}RECORD;
