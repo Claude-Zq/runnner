244bb75d1c9a0820a532ca1a0d31524e400aedcc
Claude-Zq <1574089391@qq.com>
2022-05-25 15:28:17 +0800

    完善了所有查找功能
    展示查找结果是将展示分类编号换为展示分类名称
    修正力按日期查找会忽略边界日期的问题

--- api.c
+++ api.c
@@ -23,7 +23,7 @@ int* SelectRecordsByDate(DATE*a,DATE*b){
     *ret = 0;
     for (int i = 0;i<recordsArray.size;i++) {
         //满足条件，记录索引
-        if (DateCmp(a, &recordsArray.records[i].date) > 0 && DateCmp(&recordsArray.records[i].date, b) > 0) {
+        if (DateCmp(a, &recordsArray.records[i].date) >= 0 && DateCmp(&recordsArray.records[i].date, b) >= 0) {
             ret[0]++;
             //重新分配内存
             ret = (int *) realloc(ret, ret[0] + 1);
@@ -100,20 +100,20 @@ int* SelectRecordsByCate(int cateID){
 
 void ShowRecords(const int *ret){
     if (ret[0]==0) {
-        printf("无满足条件的记录\n");
+        printf("无满足条件的记录!\n");
         return;
     }
     else{
-        printf("===查询结果如下===\n");
-        printf("   %s    \t %s \t%s\t   %s\n","日期","时长","距离","类别");
+        printf("\t\t===查询结果如下===\n");
+        printf("    日期     时长     距离       类别\n");
         for (int i = 1;i<=ret[0];i++){
-            printf("%04d-%02d-%02d\t%4.2f\t%4.2f\t%d\n",
+            printf("%04d-%02d-%02d \t%4.2f\t%4.2f\t%s\n",
                    recordsArray.records[ret[i]].date.year,
                    recordsArray.records[ret[i]].date.month,
                    recordsArray.records[ret[i]].date.day,
                    recordsArray.records[ret[i]].duration,
                    recordsArray.records[ret[i]].distance,
-                   recordsArray.records[ret[i]].category);
+                   cateIdToString[recordsArray.records[ret[i]].category]);
         }
     }
     free(ret);//释放堆区开辟的内存
@@ -238,6 +238,7 @@ void SelectRecords() {
         }
         default:
             printf("无该选项！");
+            return;
     }
     ShowRecords(ret);
 }
